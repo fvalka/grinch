02ded4a69fc2f55610712b3a43a48462f183f250
aineniamh <aine.otoole@ed.ac.uk>
2021-01-16 15:00:39 +0000

    updating runner

--- grinch/scripts/grinch_runner.sh
+++ grinch/scripts/grinch_runner.sh
@@ -4,7 +4,7 @@ TODAY=$(date +%F_%H%M%S)
 
 git pull #gets any updates to the reports in the data directory
 
-grinch -t 10 -i grinch/data/config.yaml --outdir $TODAY --output-prefix global_report
+grinch -t 10 -i /home/shared/grinch/grinch/data/config.yaml --outdir $TODAY --output-prefix global_report
 
 cp $TODAY/report/global_report_B.1.1.7.html /home/shared/lineages-website/data/global_report.B.1.1.7.html
 cp $TODAY/report/global_report_B.1.351.html /home/shared/lineages-website/data/global_report.B.1.351.html
