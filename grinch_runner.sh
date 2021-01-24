c571349c1477c7f6172b33426c8f275c42819eef
aineniamh <aine.otoole@ed.ac.uk>
2021-01-24 17:16:03 +0000

    updating runner

--- grinch_runner.sh
+++ grinch_runner.sh
@@ -14,22 +14,22 @@ cd /home/shared/lineages-website && git pull
 
 python /home/shared/grinch/grinch/scripts/update_website.py --website-dir /home/shared/lineages-website -m /home/shared/$TODAY/2/lineages.metadata.csv -n /home/shared/pangoLEARN/pangoLEARN/supporting_information/lineage_notes.txt -o /home/shared/lineages-website/_data/lineage_data.json
 
-cp /home/shared/$TODAY/report/global_report_B.1.1.7.html /home/shared/lineages-website/global_report_B.1.1.7.html
-cp /home/shared/$TODAY/report/global_report_B.1.351.html /home/shared/lineages-website/global_report_B.1.351.html
-cp /home/shared/$TODAY/report/global_report_P.1.html /home/shared/lineages-website/global_report_P.1.html
+cp /home/shared/$TODAY/report/global_report_B.1.1.7.md /home/shared/lineages-website/global_report_B.1.1.7.md
+cp /home/shared/$TODAY/report/global_report_B.1.351.md /home/shared/lineages-website/global_report_B.1.351.md
+cp /home/shared/$TODAY/report/global_report_P.1.md /home/shared/lineages-website/global_report_P.1.md
+cp /home/shared/$TODAY/report/grinch_data.json /home/shared/lineages-website/_data/grinch_data.json
 
-cp /home/shared/$TODAY/figures/Map_of_B.1.351_local_transmission.svg /home/shared/lineages-website/assets/images/Map_of_B.1.351_local_transmission.svg
-cp /home/shared/$TODAY/figures/Map_of_B.1.1.7_local_transmission.svg /home/shared/lineages-website/assets/images/Map_of_B.1.1.7_local_transmission.svg
-cp /home/shared/$TODAY/figures/Map_of_P.1_local_transmission.svg /home/shared/lineages-website/assets/images/Map_of_P.1_local_transmission.svg
+cp /home/shared/$TODAY/figures/*.svg /home/shared/lineages-website/assets/images/
 
 cp /home/shared/$TODAY/2/lineages.metadata.csv /home/shared/lineages-website/_data/lineages.metadata.csv
 
 cd /home/shared/lineages-website && git pull
 
-git add /home/shared/lineages-website/global_report_*.html
+git add /home/shared/lineages-website/global_report_*.md
 git add /home/shared/lineages-website/_data/lineages.metadata.csv
 git add /home/shared/lineages-website/_data/lineage_data.json
-git add /home/shared/lineages-website/assets/images/Map_of*loca*
+git add /home/shared/lineages-website/_data/grinch_data.json
+git add /home/shared/lineages-website/assets/images/*.svg
 
 git commit -m "updating new variant report $TODAY"
 git push
